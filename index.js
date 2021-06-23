45aa55e098cdefb5dc56c23855ec183dec1cf212
Louis Lassegue <louislassegue@gmail.com>
2021-06-22 20:25:50 -0700

    pass 2 test

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+require('dotenv').config;
+// Start up DB Server
+const { db } = require('./src/auth/models/index.js');
+const server = require('./src/server.js');
+const PORT = process.env.PORT || 3000;
+db.sync()
+  .then(() => {
+    // Start the web server
+    server.start(PORT);
+  });
+
